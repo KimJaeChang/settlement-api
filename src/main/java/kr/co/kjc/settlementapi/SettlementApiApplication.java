17473d3c165177468c254e9528362591bbce1c82
wockd3325 <wockd3325@gmail.com>
2024-03-12 03:17:57 +0900

    initial commit

---
+++ src/main/java/kr/co/kjc/settlementapi/SettlementApiApplication.java
@@ -0,0 +1,13 @@
+package kr.co.kjc.settlementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettlementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SettlementApiApplication.class, args);
+    }
+
+}
