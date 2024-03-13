af34da3ab3c024809e1fb907b0734a70a51c8927
KimJaeChang <wockd3325@gmail.com>
2024-03-13 16:22:39 +0900

    수정중

--- src/main/java/kr/co/kjc/settlement/web/controller/BaseController.java
+++ src/main/java/kr/co/kjc/settlement/web/controller/BaseController.java
@@ -1,5 +1,9 @@
 package kr.co.kjc.settlement.web.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -14,6 +18,24 @@ public class BaseController {
   @GetMapping("/v1/toss-payments")
   public String tossPayments() {
     return "/toss_payments/checkout";
+  }
+
+  @GetMapping("/v1/toss-payments/payment-key/return")
+  public String tossPaymentKeyReturn(HttpServletRequest request) {
+
+    System.out.println("\t");
+    System.out.println("request : " + request.getParameterNames());
+    System.out.println("\t");
+
+    Arrays.stream(new Enumeration[]{request.getParameterNames()})
+        .collect(Collectors.toList())
+        .forEach((f) -> {
+          System.out.println("\t");
+          System.out.println("f : " + f);
+          System.out.println("\t");
+        });
+
+    return null;
   }
 
 }
