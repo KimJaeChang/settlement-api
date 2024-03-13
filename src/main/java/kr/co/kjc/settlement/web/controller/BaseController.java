7fd6d6d27257798b8d3656d2005910d25f5864b5
KimJaeChang <wockd3325@gmail.com>
2024-03-13 16:43:37 +0900

    수정중

--- src/main/java/kr/co/kjc/settlement/web/controller/BaseController.java
+++ src/main/java/kr/co/kjc/settlement/web/controller/BaseController.java
@@ -20,11 +20,29 @@ public class BaseController {
     return "/toss_payments/checkout";
   }
 
-  @GetMapping("/v1/toss-payments/payment-key/return")
-  public String tossPaymentKeyReturn(HttpServletRequest request) {
+  @GetMapping("/v1/toss-payments/success")
+  public String tossPaymentKeyReturnSuccess(HttpServletRequest request) {
 
     System.out.println("\t");
-    System.out.println("request : " + request.getParameterNames());
+    System.out.println("success-request : " + request.getParameterNames());
+    System.out.println("\t");
+
+    Arrays.stream(new Enumeration[]{request.getParameterNames()})
+        .collect(Collectors.toList())
+        .forEach((f) -> {
+          System.out.println("\t");
+          System.out.println("f : " + f);
+          System.out.println("\t");
+        });
+
+    return null;
+  }
+
+  @GetMapping("/v1/toss-payments/fail")
+  public String tossPaymentKeyReturnFail(HttpServletRequest request) {
+
+    System.out.println("\t");
+    System.out.println("fail-request : " + request.getParameterNames());
     System.out.println("\t");
 
     Arrays.stream(new Enumeration[]{request.getParameterNames()})
