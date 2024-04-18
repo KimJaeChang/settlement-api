757db7ab582d07edb96ce8b4294568ae7f32ca5b
wockd3325 <wockd3325@gmail.com>
2024-04-18 17:57:32 +0900

    - RestClient 추가중

--- src/main/java/kr/co/kjc/settlement/global/config/RestClientConfig.java
+++ src/main/java/kr/co/kjc/settlement/global/config/RestClientConfig.java
@@ -1,10 +1,10 @@
 package kr.co.kjc.settlement.global.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @Configuration
 @RequiredArgsConstructor
@@ -17,5 +17,14 @@ public class RestClientConfig {
     return RestClient.create(uri)
         .mutate()
         .build();
+  }
+
+  public static RestClient create(final String uri) {
+
+    RestClient restClient = RestClient.builder().baseUrl(uri).build();
+    RestClientAdapter adapter = RestClientAdapter.create(restClient);
+    HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
+
+    return factory.createClient(RestClient.class);
   }
 }
