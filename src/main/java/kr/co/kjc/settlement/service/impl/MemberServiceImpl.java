b97a907758b44e226cac2596df2d815f60189c88
wockd3325 <wockd3325@gmail.com>
2024-04-23 11:02:55 +0900

    - 디렉토리 변경 - JWT 토근 발급 구현중

--- src/main/java/kr/co/kjc/settlement/service/impl/MemberServiceImpl.java
+++ src/main/java/kr/co/kjc/settlement/service/impl/MemberServiceImpl.java
@@ -4,7 +4,7 @@ import kr.co.kjc.settlement.global.dtos.MemberDTO;
 import kr.co.kjc.settlement.global.dtos.request.BaseSearchDTO;
 import kr.co.kjc.settlement.global.enums.EnumErrorCode;
 import kr.co.kjc.settlement.global.exception.BaseAPIException;
-import kr.co.kjc.settlement.repository.jpa.MemberRepository;
+import kr.co.kjc.settlement.repository.jpa.MemberJpaRepository;
 import kr.co.kjc.settlement.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -15,17 +15,17 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
 
-  private final MemberRepository memberRepository;
+  private final MemberJpaRepository memberJpaRepository;
 
   @Override
   public Page<MemberDTO> findAll(BaseSearchDTO dto) {
-    return memberRepository.findAll(PageRequest.of(dto.getPageNo(), dto.getPageSize()))
+    return memberJpaRepository.findAll(PageRequest.of(dto.getPageNo(), dto.getPageSize()))
         .map(MemberDTO::toDto);
   }
 
   @Override
   public MemberDTO findById(Long id) {
-    return MemberDTO.toDto(memberRepository.findById(id)
+    return MemberDTO.toDto(memberJpaRepository.findById(id)
         .orElseThrow(() -> new BaseAPIException(EnumErrorCode.NOT_FOUND_MEMBER)));
   }
 }
