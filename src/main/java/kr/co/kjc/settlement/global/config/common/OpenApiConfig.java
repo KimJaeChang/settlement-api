81273d24b5bc314c7e3eb818b352b46d9b6356a4
wockd3325 <wockd3325@gmail.com>
2024-05-23 13:53:28 +0900

    - swagger 어노테이션 공통화 진행중

--- src/main/java/kr/co/kjc/settlement/global/config/common/OpenApiConfig.java
+++ src/main/java/kr/co/kjc/settlement/global/config/common/OpenApiConfig.java
@@ -7,6 +7,8 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
 import kr.co.kjc.settlement.global.constants.CommonConstants;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -28,6 +30,14 @@ public class OpenApiConfig {
         .version(springdocVersion)
         .description(appName + " API Description");
 
+    Server server1 = new Server()
+        .url("https://settlement-api-1.co.kr");
+
+    Server server2 = new Server()
+        .url("https://settlement-api-2.co.kr");
+
+    List<Server> servers = List.of(server1, server2);
+
     String schemeName = CommonConstants.REQ_HEADER_KEY_AUTH;
     SecurityRequirement securityRequirement = new SecurityRequirement().addList(schemeName);
     Components components = new Components()
@@ -37,6 +47,7 @@ public class OpenApiConfig {
 
     return new OpenAPI()
         .info(info)
+        .servers(servers)
         .addSecurityItem(securityRequirement)
         .components(components);
   }
